d2eb68b03e908ca1f50b5d8d2778606b523a37aa
Felipe Mesquita <fmesquitacunha@gmail.com>
2009-02-02 16:24:31 -0200

    Improving the view generator. Options are expected to generate glade templates.

--- rugui_generators/view/view_generator.rb
+++ rugui_generators/view/view_generator.rb
@@ -2,9 +2,10 @@ class ViewGenerator < RubiGen::Base
 
   default_options :author => nil
 
-  attr_reader :name, :toplevel
+  attr_reader :name, :toplevel, :uses_glade
 
   def initialize(runtime_args, runtime_options = {})
+    @uses_glade = true
     super
     usage if args.empty?
     @name = args.shift
@@ -14,11 +15,13 @@ class ViewGenerator < RubiGen::Base
   def manifest
     record do |m|
       # app/views
-      m.template "view.erb", "app/views/#{@name.downcase}_view.rb", :assigns => { :view_name => @name.capitalize }
+      m.template "view.erb", "app/views/#{@name.downcase}_view.rb", :assigns => { :view_name => @name.capitalize, :uses_glade => uses_glade? }
       # app/views/helpers
       m.template "view_helper.erb", "app/views/helpers/#{@name.downcase}_view_helper.rb", :assigns => { :view_name => @name.capitalize }
       # app/resources/glade
-      m.file glade_template, "app/resources/glade/#{@name.downcase}_view.glade"
+      if uses_glade?
+        m.file glade_template, "app/resources/glade/#{@name.downcase}_view.glade"
+      end
     end
   end
 
@@ -33,6 +36,8 @@ EOS
 
     def add_options!(opts)
       opts.separator ' '
+      opts.on("-x", "--without-glade", "Create the view without a glade.") { |o| @uses_glade = false }
+      opts.on("-e", "--default-glade", "Create the view with a default glade.") { |o| @toplevel = nil }
       opts.on("-w", "--window", option_phrase_for('window')) { |o| @toplevel = "window" }
       opts.on("-d", "--dialog-box", option_phrase_for('dialog box')) { |o| @toplevel = "dialog_box" }
       opts.on("-a", "--about-dialog", option_phrase_for('about dialog')) { |o| @toplevel = "about_dialog" }
@@ -48,12 +53,16 @@ EOS
     end
 
     def option_phrase_for(toplevel)
-      "Create the glade file using #{toplevel} template as toplevel."
+      "Create the view with a glade using #{toplevel} template as toplevel."
     end
 
     # Gets the correct glade template.
     def glade_template
       return "view.glade" if @toplevel.nil?
       return "toplevels/#{@toplevel}.glade"
+    end
+
+    def uses_glade?
+      !!@uses_glade
     end
 end
