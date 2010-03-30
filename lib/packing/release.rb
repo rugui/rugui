943edb315cdeb83cba012efd14857eba3e9731ce
Vicente Mundim <vicente.mundim@gmail.com>
2010-03-30 16:12:51 -0300

    Bumping version number and adding Cole Teeter as contributor

--- lib/packing/release.rb
+++ lib/packing/release.rb
@@ -3,13 +3,13 @@ begin
   Jeweler::Tasks.new do |gemspec|
     gemspec.name = "rugui"
     gemspec.summary = "A simple MVC framework for RubyGTK."
-    gemspec.email = ["vicente.mundim@gmail.com", "fmesquitacunha@gmail.com", "claudioe@gmail.com"]
+    gemspec.email = ["vicente.mundim@gmail.com", "fmesquitacunha@gmail.com", "claudioe@gmail.com", "thecatwasnot@gmail.com"]
     gemspec.homepage = "http://rugui.org"
     gemspec.description = "A simple MVC framework for RubyGTK."
-    gemspec.authors = ["Vicente Mundim", "Felipe Mesquita", "Claudio Escudero"]
+    gemspec.authors = ["Vicente Mundim", "Felipe Mesquita", "Claudio Escudero", "Cole Teeter"]
     gemspec.add_dependency(%q<activesupport>, [">= 2.1.1"])
     gemspec.add_dependency(%q<thor>, [">= 0.13.3"])
-    gemspec.date = %q{2010-02-19}
+    gemspec.date = %q{2010-03-30}
     gemspec.rubyforge_project = "rugui"
     gemspec.executables = ['rugui']
     gemspec.files = FileList["bin/*", "lib/**/*", "script/*", "Changelog", "LICENSE", "Rakefile", "README"].to_a
