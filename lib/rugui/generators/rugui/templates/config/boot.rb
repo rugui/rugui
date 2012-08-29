e9a02c5520ca276debded5e974dd3bf08f5c376b
Vicente Mundim <vicente.mundim@gmail.com>
2012-08-29 09:32:24 -0300

    Adding APPLICATION_ROOT configuration to boot and RSpec gem to application Gemfile

--- lib/rugui/generators/rugui/templates/config/boot.rb
+++ lib/rugui/generators/rugui/templates/config/boot.rb
@@ -3,4 +3,6 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+APPLICATION_ROOT = File.expand_path('../..', __FILE__) unless defined?(APPLICATION_ROOT)
+
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
